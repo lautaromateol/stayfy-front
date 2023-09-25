31714e98d32edf8208987bd736e680be1fbd2ae5
lautaromateol <lautaromateol@gmail.com>
2023-09-25 15:01:44 -0300

    api añadida y configuracion de server

--- back/index.js
+++ back/index.js
@@ -1,9 +1,9 @@
 const server = require("./src/server");
-const { conn } = require('./src/db.js');
+//const { conn } = require('./src/db.js');
 const PORT = 3001;
 
-conn.sync({ force: false }).then(() => {
+//conn.sync({ force: false }).then(() => {
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 })
-}).catch(error => console.error(error))
+//}).catch(error => console.error(error))
