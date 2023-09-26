5ea9b73e81d99d60382098829c480c8819e0d6fa
juane <juane.elizondo@yahoo.com>
2023-09-26 19:48:17 -0300

    solo falta agregar estilos al form

--- front/src/views/Create/Create.jsx
+++ front/src/views/Create/Create.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import validation from './validation/validation';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch} from 'react-redux';
 import { getBooks } from '../../redux/actions' 
 
 
