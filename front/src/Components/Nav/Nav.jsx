638a4496bcfd8e3fb3b30b936680e3b81d91c4f4
mhelueni <mhelueni@gmail.com>
2023-09-27 01:23:14 -0300

    4

--- front/src/Components/Nav/Nav.jsx
+++ front/src/Components/Nav/Nav.jsx
@@ -1,10 +1,14 @@
 import SearchBar from "../SearchBar/SearchBar"
 import style from "./Nav.module.css"
+import {Link} from "react-router-dom"
 
 const Nav = ()=>{
     return (
         <div className={style.nav}>
             <button>Books</button>
+            <button>
+                <Link to="/create">Create</Link>
+            </button>
             <SearchBar />
             <button>Log In</button>
         </div>
