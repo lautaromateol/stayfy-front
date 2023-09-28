6c17e9ff135f7b2a5f819e4c4daa68a4b0cfbd64
mhelueni <mhelueni@gmail.com>
2023-09-28 13:06:11 -0300

    10

--- front/src/Components/Nav/Nav.jsx
+++ front/src/Components/Nav/Nav.jsx
@@ -1,10 +1,12 @@
 import SearchBar from "../SearchBar/SearchBar"
-import style from "./Nav.module.css"
 import {Link} from "react-router-dom"
 
 const Nav = ()=>{
     return (
-        <div className={style.nav}>
+        <div>
+            <button>
+                <Link to="/">Home</Link>
+            </button>
             <button>Books</button>
             <button>
                 <Link to="/create">Create</Link>
--- front/src/Views/Home/Home.jsx
+++ front/src/Views/Home/Home.jsx
@@ -1,11 +1,11 @@
 import CardList from "../../Components/CardList/cardList"
-import Nav from "../../Components/Nav/Nav"
+// import Nav from "../../Components/Nav/Nav"
 
 const Home = ()=>{
     return (
         <div>
-            <Nav></Nav>
             <h1>Home</h1>
+            <img src="https://banner2.cleanpng.com/20180316/chq/kisspng-book-free-content-clip-art-office-books-cliparts-5aab4c121e0864.187068161521175570123.jpg" />
             <CardList />
         </div>
     )
