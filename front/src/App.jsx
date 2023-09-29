f2bcb4755e7d13b22149ec896af073f37e60fb63
lautaromateol <lautaromateol@gmail.com>
2023-09-29 15:53:24 -0300

    post order tab added

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
-import './App.css'
 import Home from './Views/Home/Home'
 import Create from './Views/Create/Create'
 import Detail from './Components/Detail/Detail'
 import Nav from './Components/Nav/Nav'
-import LogIn from './Views/LogIn/LogIn'
-import Books from './Views/Books/books'
+import LogIn from "./Views/LogIn/LogIn"
+import Books from "./Views/Books/books"
+import Success from './Components/Success/Success'
+import './App.css'
 
 function App() {
 
@@ -16,8 +17,9 @@ function App() {
           <Route path='/' element= {<Home/>}/>
           <Route path='/create' element={<Create/>}/>
           <Route path='/product-page/:id' element={<Detail/>}/>
-          <Route path='/login' element={<LogIn/>}></Route>
-          {/* <Route path='/books' element={<Books/>}></Route> */}
+          <Route path='/login' element={<LogIn/>}/>
+          <Route path='/books' element={<Books/>}/>
+          <Route path='/order-approved' element={<Success/>}/>
         </Routes>
       </div>
   )
