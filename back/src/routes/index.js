05cef406817e0a9ff116c8cc1bf1d62e95a67eda
lautaromateol <lautaromateol@gmail.com>
2023-10-04 16:11:52 -0300

    ruta usuarios añadida

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -5,6 +5,7 @@ const { booksRouter } = require("./booksRoutes");
 const { genresRouter } = require("./genresRoutes");
 const { mercadopagoRouter } = require("./mp-router");
 const { orderRouter } = require("./orderRoutes");
+const { userRouter } = require('./userRouter')
 
 const router = Router();
 
@@ -15,5 +16,6 @@ router.use('/books', booksRouter);
 router.use('/genres', genresRouter);
 router.use('/checkout/mercado-pago', mercadopagoRouter)
 router.use('/orders', orderRouter)
+router.use('/users', userRouter)
 
 module.exports = router;
