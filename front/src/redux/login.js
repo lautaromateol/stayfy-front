1e9d0613b9227c733132bd3bd7518851a19a20db
lautaromateol <lautaromateol@gmail.com>
2023-10-17 21:45:14 -0300

    login action modificado

--- front/src/redux/login.js
+++ front/src/redux/login.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const login = async credentials => {
-    const { data } = await axios.post("http://localhost:3001/users/login", credentials)
+    const { data } = await axios.post(`${BACKEND_URL}/users/login`, credentials)
     return data
 }
 
