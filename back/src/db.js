8a543dcb05b121f9eecd2b79a37155028db91e05
jfi1000 <jf.izaguirre@gmail.com>
2023-10-18 20:55:02 -0600

    review_rediseño para actualizar rating en books

--- back/src/db.js
+++ back/src/db.js
@@ -74,7 +74,8 @@ module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
   createDefaultAdminUser,
-  User
+  User,
+  Book
 };
 
 
