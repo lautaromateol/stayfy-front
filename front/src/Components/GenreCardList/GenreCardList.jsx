b3702e191fa604cad9a957403a884fcaaf836ae7
lautaromateol <lautaromateol@gmail.com>
2023-11-07 20:01:08 -0300

    genre card list

--- front/src/Components/GenreCardList/GenreCardList.jsx
+++ front/src/Components/GenreCardList/GenreCardList.jsx
@@ -1,16 +1,26 @@
 import CardV2 from "../Cards/CardV2";
-import { useSelector } from "react-redux";
+import Slider from 'react-slick';
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from 'react-router-dom';
 import { useEffect } from "react";
 import Aos from 'aos';
 import 'aos/dist/aos.css';
-import { useParams } from 'react-router-dom';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const GenreCardList = () => {
-  const { genre } = useParams(); 
+
+  const { genre } = useParams();
+
   const books = useSelector((state) => state.books);
 
   useEffect(() => {
-    Aos.init({duration: 1500});
+    Aos.init({ duration: 1000 });
+    const lastTab = localStorage.getItem('lastTab')
+      if(lastTab){
+          localStorage.removeItem('lastTab')
+          localStorage.setItem('lastTab', window.location.href)
+      }
   }, []);
 
   const filteredBooks = genre
@@ -18,28 +28,24 @@ const GenreCardList = () => {
     : books;
 
   return (
-    <div class="h-full">
-      <section
-        x-data="xData()"
-        className="bg-stone-400 dark:bg-gray-900 py-10 px-12"
-      >
-        <div
+    <div class="min-h-screen w-full bg-gray-100 dark:bg-gray-900 py-10 px-12">
+      <h1 class="mb-5 text-center text-3xl font-bold">{genre}</h1>
+      <div
           className="grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
           data-aos="fade-up"
-        >
-          {filteredBooks.map(({ id, title, authors, price, image, rating }) => (
-            <CardV2
-              key={id}
-              id={id}
-              title={title}
-              authors={authors}
-              image={image}
-              price={price}
-              rating={rating}
-            />
-          ))}
-        </div>
-      </section>
+      >
+        {filteredBooks.map(({ id, title, authors, price, image, rating }) => (
+          <CardV2
+            key={id}
+            id={id}
+            title={title}
+            authors={authors}
+            image={image}
+            price={price}
+            rating={rating}
+          />
+        ))}
+      </div>
     </div>
   );
 }
